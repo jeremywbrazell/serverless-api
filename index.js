02808e7d42f3c436a871e72c5f4cb7490d0c5771
jeremywbrazell <jwbrazell@gmail.com>
2021-11-24 13:54:07 -0800

    pushing lab18

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let uuid = require('uuid').v4;
+let peopleModel = require('./people.schema');
+
+exports.handler = async (event) => {
+
+  console.log('EVENT', event);
+
+  if (event.httpMethod === 'POST') {
+    try {
+      const { name, phone } = JSON.parse(event.body);
+      const id = uuid();
+
+      const record = new peopleModel({ id, name, phone });
+      console.log('RECORD', record);
+
+      const data = await record.save();
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+
+    } catch (err) {
+      return {
+        statusCode: 500,
+        response: err.message,
+      };
+    }
+  } else if (event.httpMethod === 'GET') {
+    try {
+      // either there is or is not an id
+      const id = event.pathParameters && event.pathParameters.id;
+
+      let data;
+
+      if (id) {
+        const list = await peopleModel.query('id').eq(id).exec();
+        console.log('LIST', list);
+        data = list[0];
+      } else {
+        data = await peopleModel.scan().exec();
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      }
+
+    } catch (err) {
+      return {
+        statusCode: 500,
+        response: err.message
+      }
+    }
+  }
+
+};
