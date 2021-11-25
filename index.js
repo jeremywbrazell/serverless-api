f47fff3416405d8a067b391b254ac01fa214e385
jeremywbrazell <jwbrazell@gmail.com>
2021-11-24 16:03:11 -0800

    testing update

--- index.js
+++ index.js
@@ -53,6 +53,31 @@ exports.handler = async (event) => {
         response: err.message
       }
     }
+  }else if (event.httpMethod === 'PUT') {
+    try {
+      // either there is or is not an id
+      const id = event.pathParameters && event.pathParameters.id;
+
+      let data;
+
+      if (id) {
+        const list = await peopleModel.update({ id }, { name: 'joe'} ).eq(id).exec();
+        console.log('LIST', list);
+        data = list[0];
+      } else {
+        data = await peopleModel.scan().exec();
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      }
+
+    } catch (err) {
+      return {
+        statusCode: 500,
+        response: err.message
+      }
+    }
   }
 
 };
